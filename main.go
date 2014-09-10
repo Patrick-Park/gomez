880d3aa05f67d4912e303b8919a3dab8ccdfd8a8
gabriel-aszalos <gabriel@stripetree.co.uk>
2014-09-10 16:37:45 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/textproto"
+	"strconv"
+	"strings"
+)
+
+var port = flag.Int("port", 25, "specifies the port that the SMTP server starts on")
+
+func handshake(conn net.Conn) {
+	fmt.Printf("[SMTP] Connected: %s\n", conn.RemoteAddr())
+
+	text := textproto.NewConn(conn)
+	defer text.Close()
+
+	for {
+		cmd, err := text.ReadLine()
+		if err != nil {
+			log.Printf("Error: %s", err)
+			break
+		}
+
+		if strings.ToUpper(cmd) == "BYE" {
+			fmt.Printf("[SMTP] Disconnected %s\n", conn.RemoteAddr())
+			break
+		}
+
+		fmt.Println("Received: " + cmd)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	ln, err := net.Listen("tcp", ":"+strconv.Itoa(*port))
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	fmt.Printf("[SMTP] Listening on port :%d\n", *port)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("Error: %s", err)
+		}
+
+		go handshake(conn)
+	}
+}
