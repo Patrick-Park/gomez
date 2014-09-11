1cbabcbc1ea3ed00f226e0a2dd1fb1033c15eb1a
gabriel-aszalos <gabriel@stripetree.co.uk>
2014-09-10 23:35:45 -0700

    Compact etsts

--- message_test.go
+++ message_test.go
@@ -11,41 +11,13 @@ func TestAddressCreation(t *testing.T) {
 		expct    Address // Expected result address
 		err      bool    // Expect error
 	}{
-		{
-			`"John Doe" <jerry@seinfeld.com>`,
-			Address{"John Doe", "jerry", "seinfeld.com"},
-			false,
-		},
-		{
-			`"John" jerry@seinfeld.com>`,
-			Address{},
-			true,
-		},
-		{
-			`"John" jerry@seinfeld.com`,
-			Address{},
-			true,
-		},
-		{
-			`sosme random" <qwe@asd.com>`,
-			Address{},
-			true,
-		},
-		{
-			`"John" jerry@seinfeld.com>`,
-			Address{},
-			true,
-		},
-		{
-			`"John" <jerry@seinfeld.com>`,
-			Address{"John", "jerry", "seinfeld.com"},
-			false,
-		},
-		{
-			`"" <>`,
-			Address{},
-			true,
-		},
+		{`"John Doe" <jerry@seinfeld.com>`, Address{"John Doe", "jerry", "seinfeld.com"}, false},
+		{`"John" jerry@seinfeld.com>`, Address{}, true},
+		{`"John" jerry@seinfeld.com`, Address{}, true},
+		{`sosme random" <qwe@asd.com>`, Address{}, true},
+		{`"John" jerry@seinfeld.com>`, Address{}, true},
+		{`"John" <jerry@seinfeld.com>`, Address{"John", "jerry", "seinfeld.com"}, false},
+		{`"" <>`, Address{}, true},
 	}
 
 	for _, test := range testCases {
