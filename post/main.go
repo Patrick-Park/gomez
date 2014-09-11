fe72045a1dafb2fa82fc03cccfa2ff36b24a7760
gabriel-aszalos <gabriel@stripetree.co.uk>
2014-09-10 21:23:55 -0700

    Mailbox data structures

--- post/main.go
+++
@@ -1,5 +0,0 @@
-package post
-
-type Mailbox interface {
-	Put()
-}
--- smtp/main.go
+++
@@ -1,8 +0,0 @@
-package smtp
-
-import "github.com/gbbr/gomez/post"
-
-type Config struct {
-	Mailbox post.Mailbox
-	Port    int
-}
