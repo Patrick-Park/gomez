2b23695fe9eb3c6edc5ade0b6a86a219239c86c1
gabriel-aszalos <gabriel@stripetree.co.uk>
2014-09-10 17:09:54 -0700

    Added

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"net/textproto"
 	"strconv"
 	"strings"
+
+	_ "github.com/gbbr/gomez/smtp"
 )
 
 var port = flag.Int("port", 25, "specifies the port that the SMTP server starts on")
@@ -40,6 +42,7 @@ func main() {
 	ln, err := net.Listen("tcp", ":"+strconv.Itoa(*port))
 	if err != nil {
 		log.Printf("Error: %s", err)
+		return
 	}
 
 	fmt.Printf("[SMTP] Listening on port :%d\n", *port)
