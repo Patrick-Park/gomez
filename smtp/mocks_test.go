9f097f74f74dc0c7be9824d39649659ff95e57c0
gabriel-aszalos <gabriel@stripetree.co.uk>
2014-09-12 23:38:37 -0700

    no mo mock

--- smtp/mocks_test.go
+++ smtp/mocks_test.go
@@ -1,16 +1,5 @@
 package smtp
 
-// mock net.Conn
 // mock CommandSpec
 // mock Mailbox
-
-// type Conn interface {
-//         Read(b []byte) (n int, err error)
-//         Write(b []byte) (n int, err error)
-//         Close() error
-//         LocalAddr() Addr
-//         RemoteAddr() Addr
-//         SetDeadline(t time.Time) error
-//         SetReadDeadline(t time.Time) error
-//         SetWriteDeadline(t time.Time) error
-// }
+// Use net.Pipe() to mock 2 connection ends
