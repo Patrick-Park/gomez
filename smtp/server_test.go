97db5a8d2f6af89facad2168693caa2e93b69b63
gabriel-aszalos <gabriel@stripetree.co.uk>
2014-09-18 23:25:56 +0100

    Moved mock to mocked object's test

--- smtp/server_test.go
+++ smtp/server_test.go
@@ -7,6 +7,40 @@ import (
 	"testing"
 )
 
+var _ MailService = new(MockMailService)
+
+// Mocks the MailService interface by exposing
+// overridable functions
+type MockMailService struct {
+	Run_      func(*Client, string) error
+	Digest_   func(*Client) error
+	Settings_ func() Config
+}
+
+func (h MockMailService) Run(c *Client, m string) error {
+	if h.Run_ != nil {
+		return h.Run_(c, m)
+	}
+
+	return nil
+}
+
+func (h MockMailService) Digest(c *Client) error {
+	if h.Digest_ != nil {
+		return h.Digest_(c)
+	}
+
+	return nil
+}
+
+func (h MockMailService) Settings() Config {
+	if h.Settings_ != nil {
+		return h.Settings_()
+	}
+
+	return Config{}
+}
+
 // Should correctly run commands from spec, echo parameters
 // and reject bad commands or commands that are not in the spec
 func TestServerRun(t *testing.T) {
