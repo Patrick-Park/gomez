38dc62a7d06f0405738885eb0a22d974251e2e03
gabriel-aszalos <gabriel@stripetree.co.uk>
2014-09-22 20:46:34 +0100

    Documentation of SMTP commands

--- smtp/commands.go
+++ smtp/commands.go
@@ -7,6 +7,10 @@ import (
 	"github.com/gbbr/gomez"
 )
 
+// RFC 2821 4.1.1.1  Extended HELLO (EHLO) or HELLO (HELO)
+// This commands are used to identify the SMTP client to the SMTP
+// server. The argument field contains the fully-qualified domain name
+// of the SMTP client if one is available.
 func cmdHELO(ctx *Client, param string) error {
 	if len(param) == 0 {
 		return ctx.Notify(Reply{501, "Syntax: HELO hostname"})
@@ -18,6 +22,10 @@ func cmdHELO(ctx *Client, param string) error {
 	return ctx.Notify(Reply{250, "Gomez SMTPd"})
 }
 
+// RFC 2821 4.1.1.1  Extended HELLO (EHLO) or HELLO (HELO)
+// This commands are used to identify the SMTP client to the SMTP
+// server. The response of EHLO is normally a multi-line one reflecting
+// the supported features of this server.
 func cmdEHLO(ctx *Client, param string) error {
 	if len(param) == 0 {
 		return ctx.Notify(Reply{501, "Syntax: EHLO hostname"})
@@ -29,6 +37,9 @@ func cmdEHLO(ctx *Client, param string) error {
 	return ctx.Notify(Reply{250, "Gomez SMTPd\nVRFY"})
 }
 
+// RFC 2821 4.1.1.2 MAIL (MAIL)
+// This command is used to initiate a mail transaction in which the
+// mail data is delivered to an SMTP server
 func cmdMAIL(ctx *Client, param string) error {
 	switch {
 	case ctx.Mode == MODE_HELO:
@@ -52,6 +63,10 @@ func cmdMAIL(ctx *Client, param string) error {
 	return ctx.Notify(Reply{250, "2.1.0 Ok"})
 }
 
+// RFC 2821 4.1.1.3 RECIPIENT (RCPT)
+// This command is used to identify an individual recipient of the mail
+// data; multiple recipients are specified by multiple use of this
+// command.
 func cmdRCPT(ctx *Client, param string) error {
 	switch {
 	case ctx.Mode == MODE_HELO:
@@ -93,6 +108,10 @@ func cmdRCPT(ctx *Client, param string) error {
 	return ctx.Notify(Reply{451, "Requested action aborted: error in processing"})
 }
 
+// RFC 2821 4.1.1.4 DATA (DATA)
+// This command causes the mail data to be appended to the mail data buffer.
+// The mail data is terminated by a line containing only a period, that is,
+// the character sequence "<CRLF>.<CRLF>"
 func cmdDATA(ctx *Client, param string) error {
 	switch ctx.Mode {
 	case MODE_HELO:
@@ -127,19 +146,32 @@ func cmdDATA(ctx *Client, param string) error {
 	return ctx.Notify(Reply{250, "Message queued"})
 }
 
+// RFC 2821 4.1.1.5 RESET (RSET)
+// This command specifies that the current mail transaction will be aborted.
+// Any stored sender, recipients, and mail data MUST be discarded, and all
+// buffers and state tables cleared.
 func cmdRSET(ctx *Client, param string) error {
 	ctx.Reset()
 	return ctx.Notify(Reply{250, "2.0.0 Ok"})
 }
 
+// RFC 2821 4.1.1.9 NOOP (NOOP)
+// This command does not affect any parameters or previously entered
+// commands.
 func cmdNOOP(ctx *Client, param string) error {
 	return ctx.Notify(Reply{250, "2.0.0 Ok"})
 }
 
+// RFC 2821 4.1.1.6 VERIFY (VRFY)
+// This command asks the receiver to confirm that the argument
+// identifies a user or mailbox.
 func cmdVRFY(ctx *Client, param string) error {
 	return ctx.Notify(Reply{252, "2.1.5 Send some mail, I'll try my best"})
 }
 
+// RFC 2821 4.1.1.10 QUIT (QUIT)
+// This command specifies that the receiver MUST send an OK reply, and
+// then close the transmission channel.
 func cmdQUIT(ctx *Client, param string) error {
 	ctx.Mode = MODE_QUIT
 	return ctx.Notify(Reply{221, "2.0.0 Adeus"})
