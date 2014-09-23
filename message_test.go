99b695ae1c014ac44561623026c3de4ed7b1948b
gabriel-aszalos <gabriel@stripetree.co.uk>
2014-09-23 18:23:41 +0100

    Message.FromRaw() tests

--- message_test.go
+++ message_test.go
@@ -24,3 +24,58 @@ func TestMessage_AddHeader(t *testing.T) {
 		t.Errorf("Got unexpected result: %#v", m.Headers)
 	}
 }
+
+func TestMessage_FromRaw(t *testing.T) {
+	testSuite := []struct {
+		raw         string
+		msg         Message
+		isCompliant bool
+	}{
+		{`From: Some guy
+Date: 23 Jan 2014
+Subject: Hi
+
+Hey what's up?`, Message{
+			Headers: OrderedHeader{textproto.MIMEHeader{
+				"From":    []string{"Some guy"},
+				"Date":    []string{"23 Jan 2014"},
+				"Subject": []string{"Hi"},
+			}},
+			Body: "Hey what's up?",
+		}, true},
+		{`Date: 1 Dec 2018
+From: Jim
+
+Hello
+Was gonna ask about you`, Message{
+			Headers: OrderedHeader{textproto.MIMEHeader{
+				"From": []string{"Jim"},
+				"Date": []string{"1 Dec 2018"},
+			}},
+			Body: "Hello\r\nWas gonna ask about you",
+		}, true},
+		{`From: Some guy
+Date: 23 Jan 2014
+Subject: Hi
+`, Message{}, false},
+		{`From: Some guy
+Subject: Hi
+
+Hey what's up?`, Message{}, false},
+		{`Hey what's up?`, Message{}, false},
+		{``, Message{}, false},
+	}
+
+	for _, test := range testSuite {
+		m := NewMessage()
+		err := m.FromRaw(test.raw)
+
+		if test.isCompliant && (!reflect.DeepEqual(m.Headers, test.msg.Headers) || test.msg.Body != m.Body) {
+			t.Errorf("Expected %#v and %s, but got \r\n\r\n%#v and %s", test.msg.Headers, test.msg.Body, m.Headers, m.Body)
+		}
+
+		if !test.isCompliant && err == nil {
+			t.Errorf("Incompliant message passed as compliant: %s", test.raw)
+		}
+	}
+}
