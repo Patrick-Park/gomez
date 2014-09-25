801aca8d67d6365ec7ad37001d9bcaf8d58bd485
gabriel-aszalos <gabriel@stripetree.co.uk>
2014-09-25 19:43:54 +0100

    8BITMIME

--- smtp/commands.go
+++ smtp/commands.go
@@ -35,7 +35,7 @@ func cmdEHLO(ctx *Client, param string) error {
 	ctx.Id = param
 	ctx.Mode = MODE_MAIL
 
-	return ctx.Notify(Reply{250, "Gomez SMTPd\nVRFY"})
+	return ctx.Notify(Reply{250, "Gomez SMTPd\n8BITMIME\nVRFY"})
 }
 
 // RFC 2821 4.1.1.2 MAIL (MAIL)
