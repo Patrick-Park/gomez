d8b3f3bc0147aab75af7686af26b66556abdf1de
gbbr <ga@stripetree.co.uk>
2014-10-16 17:48:41 +0100

    Test DB

--- enqueuer_test.go
+++ enqueuer_test.go
@@ -1,10 +1,46 @@
 package gomez
 
-import "testing"
+import (
+	"log"
+	"os"
+	"os/exec"
+	"sync"
+	"testing"
+)
 
-const dbString = "user=Gabriel dbname=gomez sslmode=disable"
+const (
+	dbUser         = "Gabriel"
+	dbString       = "user=" + dbUser + " dbname=gomez_test sslmode=disable"
+	testSchemaFile = "schema_test.sql"
+)
+
+var once sync.Once
+
+// Ensures the test database is set up. Should be called before each test
+func EnsureTestDB() {
+	once.Do(setUpTestDB)
+}
+
+// Sets up the test database from the schema file.
+func setUpTestDB() {
+	file, err := os.Open(testSchemaFile)
+	if err != nil {
+		log.Fatalf("Error opening schema file: %s", err)
+	}
+	defer file.Close()
+
+	cmd := exec.Command("psql", "--username="+dbUser, "-q")
+	cmd.Stdin = file
+
+	err = cmd.Run()
+	if err != nil {
+		log.Fatalf("Error setting up DB: %s", err)
+	}
+}
 
 func TestPostBox_DB_Connection(t *testing.T) {
+	EnsureTestDB()
+
 	pb, err := NewPostBox(dbString)
 	if err != nil {
 		t.Errorf("Could not open DB:", err)
@@ -29,6 +65,8 @@ func TestPostBox_NextID_Error(t *testing.T) {
 }
 
 func TestPostBox_NextID_Success(t *testing.T) {
+	EnsureTestDB()
+
 	pb, err := NewPostBox(dbString)
 	if err != nil {
 		t.Errorf("Could not open DB:", err)
