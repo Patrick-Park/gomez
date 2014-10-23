84ced7989957e958dfb9953d7c0a296950d1065b
gbbr <ga@stripetree.co.uk>
2014-10-23 20:59:15 +0100

    Dequeuer mock and mock-up (heh)

---
+++ mailbox/dequeuer.go
@@ -0,0 +1,25 @@
+package mailbox
+
+// Dequeuer returns jobs off the queue and updates their completion status
+type Dequeuer interface {
+	// Dequeue retrieves a given number of messages from the queue
+	Dequeue(n int) ([]*Job, error)
+
+	// Update updates one or more given jobs
+	Update(j ...Job) error
+}
+
+// Job holds a message and the recipients it needs to arrive at
+type Job struct {
+	Msg        Message // The actual message to be delivered
+	Dest       string  // The recipient address list
+	ReturnPath string  // The message return-path
+}
+
+func (p *postBox) Dequeue(n int) ([]*Job, error) {
+	return nil, nil
+}
+
+func (p *postBox) Update(j ...Job) error {
+	return nil
+}
---
+++ mailbox/dequeuer_mock.go
@@ -0,0 +1,11 @@
+package mailbox
+
+var _ Dequeuer = new(MockDequeuer)
+
+type MockDequeuer struct {
+	DequeueMock func(int) ([]*Job, error)
+	UpdateMock  func(j ...Job) error
+}
+
+func (m MockDequeuer) Dequeue(n int) ([]*Job, error) { return m.DequeueMock(n) }
+func (m MockDequeuer) Update(j ...Job) error         { return m.Update(j...) }
