9915107761fd85a7eedfc46a7ca0c00a89037ef4
gbbr <ga@stripetree.co.uk>
2014-10-25 17:41:01 +0100

    Less indent

--- mailbox/dequeuer.go
+++ mailbox/dequeuer.go
@@ -2,8 +2,9 @@ package mailbox
 
 // Dequeuer returns jobs off the queue and updates their completion status
 type Dequeuer interface {
-	// Dequeue retrieves a given number of messages from the queue
-	Dequeue(n int) ([]*Job, error)
+	// Dequeue retrieves number of jobs, up to length of passed slice. Returns
+	// number of jobs dequeued and an error.
+	Dequeue(jobs []*Job) (n int, err error)
 
 	// Update updates one or more given jobs
 	Update(j ...*Job) error
@@ -16,8 +17,8 @@ type Job struct {
 }
 
 // Dequeue will retrieve the given number of jobs ordered by date
-func (p *mailBox) Dequeue(n int) ([]*Job, error) {
-	return nil, nil
+func (p *mailBox) Dequeue(jobs []*Job) (n int, err error) {
+	return 0, nil
 }
 
 func (p *mailBox) Update(j ...*Job) error {
--- mailbox/dequeuer_mock.go
+++ mailbox/dequeuer_mock.go
@@ -4,9 +4,9 @@ var _ Dequeuer = new(MockDequeuer)
 
 // MockDequeuer is a configurable mock for the Dequeuer interface.
 type MockDequeuer struct {
-	DequeueMock func(int) ([]*Job, error)
+	DequeueMock func([]*Job) (int, error)
 	UpdateMock  func(j ...*Job) error
 }
 
-func (m MockDequeuer) Dequeue(n int) ([]*Job, error) { return m.DequeueMock(n) }
-func (m MockDequeuer) Update(j ...*Job) error        { return m.Update(j...) }
+func (m MockDequeuer) Dequeue(jobs []*Job) (int, error) { return m.DequeueMock(jobs) }
+func (m MockDequeuer) Update(j ...*Job) error           { return m.Update(j...) }
