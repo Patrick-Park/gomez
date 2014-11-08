3cec1b0896a41a9e35f03c6740e76db849fa3727
gbbr <ga@stripetree.co.uk>
2014-11-08 00:54:32 +0000

    Tidy up message

--- mailbox/message.go
+++ mailbox/message.go
@@ -19,32 +19,32 @@ type Message struct {
 	rcptOut []*mail.Address // Outbount recipients
 }
 
+// From retrieves the message Return-Path.
+func (m Message) From() *mail.Address { return m.from }
+
+// Inbound retrieves local recipients.
+func (m Message) Inbound() []*mail.Address { return m.rcptIn }
+
+// Outbound receives all out-bound recipients.
+func (m Message) Outbound() []*mail.Address { return m.rcptOut }
+
+// SetFrom adds a Return-Path given an address.
+func (m *Message) SetFrom(addr *mail.Address) { m.from = addr }
+
 // AddInbound adds a local recipient.
 func (m *Message) AddInbound(rcpt *mail.Address) {
 	m.rcptIn = append(m.rcptIn, rcpt)
 }
-
-// Inbound retrieves local recipients.
-func (m Message) Inbound() []*mail.Address { return m.rcptIn }
 
 // AddOutbound adds one or more remote recipients.
 func (m *Message) AddOutbound(rcpt ...*mail.Address) {
 	m.rcptOut = append(m.rcptOut, rcpt...)
 }
 
-// Outbound receives all out-bound recipients.
-func (m Message) Outbound() []*mail.Address { return m.rcptOut }
-
 // Rcpt returns a list of all recipients on this message.
 func (m Message) Rcpt() []*mail.Address {
 	return append(m.rcptIn, m.rcptOut...)
 }
-
-// SetFrom adds a Return-Path given an address.
-func (m *Message) SetFrom(addr *mail.Address) { m.from = addr }
-
-// From retrieves the message Return-Path.
-func (m Message) From() *mail.Address { return m.from }
 
 // Parse returns an object of type mail.Message with the headers parsed.
 func (m Message) Parse() (*mail.Message, error) {
@@ -60,14 +60,12 @@ func (m *Message) PrependHeader(name, value string, params ...interface{}) {
 // MakeAddressList returns a string parseable by mail.ParseAddressList
 func MakeAddressList(list []*mail.Address) string {
 	var r string
-
 	for i, addr := range list {
 		r += addr.String()
 		if i < len(list)-1 {
 			r += ", "
 		}
 	}
-
 	return r
 }
 
@@ -77,6 +75,5 @@ func SplitUserHost(addr *mail.Address) (user string, host string) {
 	if len(p) != 2 {
 		return
 	}
-
 	return p[0], p[1]
 }
