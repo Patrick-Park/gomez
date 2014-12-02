4f4a5c8066adddce034675b582f0dfddaeb62c13
gbbr <ga@stripetree.co.uk>
2014-12-02 18:20:44 -0500

    agent mocks

--- agent/agent.go
+++ agent/agent.go
@@ -1,6 +1,10 @@
 package agent
 
 import (
+	"fmt"
+	"log"
+	"net"
+	"sort"
 	"strconv"
 	"time"
 
@@ -21,7 +25,6 @@ func Run(dq mailbox.Dequeuer, conf jamon.Group) error {
 	}
 	tick := time.Duration(s) * time.Second
 	for {
-		cron.lastRun = <-time.After(tick)
 		jobs, err := dq.Dequeue()
 		if err != nil {
 			return err
@@ -29,9 +32,29 @@ func Run(dq mailbox.Dequeuer, conf jamon.Group) error {
 		for host, pkg := range jobs {
 			go deliverTo(host, pkg)
 		}
+		cron.lastRun = <-time.After(tick)
 	}
 }
 
-func deliverTo(host string, pkg mailbox.Delivery) {
+type byPriority []*net.MX
 
+func (b byPriority) Len() int           { return len(b) }
+func (b byPriority) Swap(i, j int)      { b[i], b[j] = b[j], b[i] }
+func (b byPriority) Less(i, j int) bool { return b[i].Pref < b[j].Pref }
+
+// lookupMX returns a list of MX hosts ordered by preference.
+// This function is declared inline so we can mock it.
+var lookupMX = func(host string) []*net.MX {
+	MXs, err := net.LookupMX(host)
+	if err != nil {
+		log.Println(err)
+	}
+	sort.Sort(byPriority(MXs))
+	return MXs
+}
+
+func deliverTo(host string, pkg mailbox.Delivery) {
+	for _, h := range lookupMX(host) {
+		fmt.Printf("%d - %s\n", h.Pref, h.Host)
+	}
 }
